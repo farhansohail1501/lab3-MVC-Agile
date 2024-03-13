63c480beda70a9c4e2176a7e8985e82802dd3183
MuhammadSohail <msohai11@my.centennialcollege.ca>
2024-03-12 23:52:01 -0400

    Add project files.

---
+++ lab3-MVC-Agile/Data/lab3_MVC_AgileContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using lab3_MVC_Agile.Models;
+
+namespace lab3_MVC_Agile.Data
+{
+    public class lab3_MVC_AgileContext : DbContext
+    {
+        public lab3_MVC_AgileContext (DbContextOptions<lab3_MVC_AgileContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<lab3_MVC_Agile.Models.Movie> Movie { get; set; } = default!;
+    }
+}
---
+++ lab3-MVC-Agile/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace lab3_MVC_Agile.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
